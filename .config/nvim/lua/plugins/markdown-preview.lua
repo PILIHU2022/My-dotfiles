ca2c286735c4ed8a30ae339bb19ab118d1bbeb3f
PILIHU2022 <2812167783@qq.com>
2023-11-19 13:45:29 +0800

    install a markdown preview plugin

---
+++ .config/nvim/lua/plugins/markdown-preview.lua
@@ -0,0 +1,8 @@
+return{
+    "iamcco/markdown-preview.nvim",
+    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+    ft = { "markdown" },
+    config = function ()
+        vim.fn["mkdp#util#install"]()
+    end
+}

4e37fcd7f0368530decaf6287cc08545fa64de08
PILIHU2022 <2812167783@qq.com>
2024-08-13 13:23:36 +0800

    Neovim-cmp:init.lua:Required a keymap file for Markdown

--- .config/nvim-use-cmp/init.lua
+++ .config/nvim-use-cmp/init.lua
@@ -1,6 +1,15 @@
+local au = vim.api.nvim_create_autocmd
 require("core.options")
 require("core.keymap")
+-- require('core.keymap_Script')
 require("lazy_nvim")
 -- require("core.ImSwitch")
 require("core.ranger")
 require("core.CodeRunning")
+au({ 'FileType' }, {
+  -- group = group,
+  pattern = 'markdown',
+  callback = function()
+    require('keymap.markdown')
+  end,
+})

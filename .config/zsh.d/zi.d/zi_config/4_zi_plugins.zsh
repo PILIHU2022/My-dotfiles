f75fec1ff70875a3f72ce0d766055c9746d0890e
PILIHU2022 <2812167783@qq.com>
2024-08-16 14:56:34 +0800

    Zi:Formatted codes

--- .config/zsh.d/zi.d/zi_config/4_zi_plugins.zsh
+++ .config/zsh.d/zi.d/zi_config/4_zi_plugins.zsh
@@ -1,16 +1,18 @@
 zi light-mode for \
   z-shell/z-a-meta-plugins \
+  junegunn/fzf \
   @annexes+ \
   @zsh-users+fast \
-  @romkatv 
+  @romkatv \
+  Aloxaf/fzf-tab
 
 # zsh-vi-mode
-# ## https://github.com/jeffreytse/zsh-vi-mode
+# https://github.com/jeffreytse/zsh-vi-mode
 zi ice depth=1;zi light jeffreytse/zsh-vi-mode     # ## zsh-vi-mode
 
 # zoxide
 zi has'zoxide' wait lucid for z-shell/zsh-zoxide   # ## 需要 zoxide 二进制文件, 如果存在则加载
 
 #  H-S-MW
-# ## https://github.com/z-shell/H-S-MW
+# https://github.com/z-shell/H-S-MW
 zi ice wait lucid; zi light z-shell/H-S-MW

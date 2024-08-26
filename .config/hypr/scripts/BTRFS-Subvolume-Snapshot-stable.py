ed78d2118c48f8c94f838daf823879e9f58b25c9
PILIHU2022 <2812167783@qq.com>
2024-08-26 12:54:53 +0800

    BTRFS:Use regex to match snapshot

--- .config/hypr/scripts/BTRFS-Subvolume-Snapshot-stable.py
+++ .config/hypr/scripts/BTRFS-Subvolume-Snapshot-stable.py
@@ -1,18 +1,16 @@
 '''Btrfs子卷快照'''
 import os
-# import time
-# from datetime import timedelta
+import re
 import datetime
 location = '/Snapshots'
-# unfull_date = time.strftime('%Y-%m', time.localtime())
-# today_dated = int(time.strftime('%d', time.localtime()))
-# yesterday_date_of_day = today_dated - 1
-# yesterday_full_date = f'{unfull_date}-{yesterday_date_of_day}'
-# today_date = time.strftime('%Y-%m-%d', time.localtime())
+ls = os.listdir(location)
+pattern = r'^\d{4}-\d{2}-\d{2}$'
+regex = re.compile(pattern)
 today_date = datetime.date.today()
 oneday = datetime.timedelta(days=1)
-yesterday = today_date-oneday
 # Btrfs delete yesterday's snapshot
-os.system(f'doas btrfs subvolume delete  / {location}/{yesterday}')
+for folder in ls:
+    if regex.match(folder):
+        os.system(f'doas btrfs subvolume delete {location}/{folder}')
 # Btrfs create today's snapshot for root(/)
 os.system(f'doas btrfs subvolume snapshot / {location}/{today_date}')

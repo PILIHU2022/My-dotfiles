b8af17756132291716a59554c63f288130c5e7d3
PILIHU2022 <2812167783@qq.com>
2024-08-30 15:14:09 +0800

    Update-Py:A few codes incomplete

--- Update.py
+++ Update.py
@@ -15,6 +15,6 @@ target_paths = [
 ]
 
 for src_path, target_path in zip(src_paths, target_paths):
-    os.system(f'rsync -a -r --quiet --delete {src_path} {target_path} --exclude=\'zi/*\'')
+    os.system(f'rsync -a -r --quiet --delete {src_path} {target_path} --exclude=\'~/.config/zsh.d/zi.d/zi/*\'')
 
 print('Finished to update')

b0c41d73d52567bccba46453ca8e8759173986e7
PILIHU2022 <2812167783@qq.com>
2024-09-17 15:50:57 +0800

    Update-Py:Ignored zi.d/zi(You will have it if you installed it)

--- Update.py
+++ Update.py
@@ -1,20 +1,26 @@
-import os
+import os,subprocess
 
 src_paths = [
-    '~/.config/hypr/',
-    '~/.config/nvim/',
-    '~/.config/zsh.d/',
-    '~/.config/kitty/',
+    "~/.config/hypr/",
+    "~/.config/nvim/",
+    "~/.config/zsh.d/",
+    "~/.config/kitty/",
 ]
 
 target_paths = [
-    '~/My-dotfiles/.config/hypr',
-    '~/My-dotfiles/.config/nvim-use-cmp',
-    '~/My-dotfiles/.config/zsh.d',
-    '~/My-dotfiles/.config/kitty',
+    "~/My-dotfiles/.config/hypr",
+    "~/My-dotfiles/.config/nvim-use-cmp",
+    "~/My-dotfiles/.config/zsh.d",
+    "~/My-dotfiles/.config/kitty",
 ]
 
 for src_path, target_path in zip(src_paths, target_paths):
-    os.system(f'rsync -a -r --quiet --delete {src_path} {target_path} --exclude=\'~/.config/zsh.d/zi.d/zi/*\'')
+    # os.system(f"rsync -a -r --quiet --delete {src_path} {target_path}")
+    if src_path == "~/.config/zsh.d/":
+        os.system(
+            f'''rsync -a -r --delete --exclude="zi.d/zi" {src_path} {target_path}'''
+        )
+    else:
+        os.system(f"rsync -a -r --quiet --delete {src_path} {target_path}")
 
-print('Finished to update')
+print("Finished to update")

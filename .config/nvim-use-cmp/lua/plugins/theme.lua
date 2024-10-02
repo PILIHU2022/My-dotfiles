35ea7d43f7fe1f37aedc98ef39fc4c950c75197c
PILIHU2022 <2812167783@qq.com>
2024-10-02 15:01:28 +0800

    Neovim-cmp:Added themes,but still use tokyonight

--- .config/nvim-use-cmp/lua/plugins/theme.lua
+++ .config/nvim-use-cmp/lua/plugins/theme.lua
@@ -1,67 +1,124 @@
 return {
+    {
+        "folke/tokyonight.nvim",
+        config = function()
+            vim.cmd [[colorscheme tokyonight-storm]]
+        end
+    },
     -- {
-    --     "folke/tokyonight.nvim",
+    --     "catppuccin/nvim",
+    --     name = "catppuccin",
+    --     priority = 1000,
     --     config = function()
-    --         vim.cmd [[colorscheme tokyonight-storm]]
+    --         require("catppuccin").setup({
+    --             flavour = "auto", -- latte, frappe, macchiato, mocha
+    --             background = {    -- :h background
+    --                 light = "latte",
+    --                 -- dark = "macchiato",
+    --                 dark = "mocha",
+    --             },
+    --             transparent_background = false, -- disables setting the background color.
+    --             show_end_of_buffer = false,     -- shows the '~' characters after the end of buffers
+    --             term_colors = false,            -- sets terminal colors (e.g. `g:terminal_color_0`)
+    --             dim_inactive = {
+    --                 enabled = false,            -- dims the background color of inactive window
+    --                 shade = "dark",
+    --                 percentage = 0.15,          -- percentage of the shade to apply to the inactive window
+    --             },
+    --             no_italic = false,              -- Force no italic
+    --             no_bold = false,                -- Force no bold
+    --             no_underline = false,           -- Force no underline
+    --             styles = {                      -- Handles the styles of general hi groups (see `:h highlight-args`):
+    --                 comments = { "italic" },    -- Change the style of comments
+    --                 conditionals = { "italic" },
+    --                 loops = {},
+    --                 functions = {},
+    --                 keywords = {},
+    --                 strings = {},
+    --                 variables = {},
+    --                 numbers = {},
+    --                 booleans = {},
+    --                 properties = {},
+    --                 types = {},
+    --                 operators = {},
+    --                 -- miscs = {}, -- Uncomment to turn off hard-coded styles
+    --             },
+    --             color_overrides = {},
+    --             custom_highlights = {},
+    --             default_integrations = true,
+    --             integrations = {
+    --                 cmp = true,
+    --                 gitsigns = true,
+    --                 nvimtree = true,
+    --                 treesitter = true,
+    --                 notify = false,
+    --                 mini = {
+    --                     enabled = true,
+    --                     indentscope_color = "",
+    --                 },
+    --                 -- For more plugins integrations please scroll down (https://github.com/catppuccin/nvim#integrations)
+    --             },
+    --         })
+    --
+    --         -- setup must be called before loading
+    --         vim.cmd.colorscheme "catppuccin"
     --     end
     -- },
-    {
-        "catppuccin/nvim",
-        name = "catppuccin",
-        priority = 1000,
-        config = function()
-            require("catppuccin").setup({
-                flavour = "auto", -- latte, frappe, macchiato, mocha
-                background = {    -- :h background
-                    light = "latte",
-                    -- dark = "macchiato",
-                    dark = "mocha",
-                },
-                transparent_background = false, -- disables setting the background color.
-                show_end_of_buffer = false,     -- shows the '~' characters after the end of buffers
-                term_colors = false,            -- sets terminal colors (e.g. `g:terminal_color_0`)
-                dim_inactive = {
-                    enabled = false,            -- dims the background color of inactive window
-                    shade = "dark",
-                    percentage = 0.15,          -- percentage of the shade to apply to the inactive window
-                },
-                no_italic = false,              -- Force no italic
-                no_bold = false,                -- Force no bold
-                no_underline = false,           -- Force no underline
-                styles = {                      -- Handles the styles of general hi groups (see `:h highlight-args`):
-                    comments = { "italic" },    -- Change the style of comments
-                    conditionals = { "italic" },
-                    loops = {},
-                    functions = {},
-                    keywords = {},
-                    strings = {},
-                    variables = {},
-                    numbers = {},
-                    booleans = {},
-                    properties = {},
-                    types = {},
-                    operators = {},
-                    -- miscs = {}, -- Uncomment to turn off hard-coded styles
-                },
-                color_overrides = {},
-                custom_highlights = {},
-                default_integrations = true,
-                integrations = {
-                    cmp = true,
-                    gitsigns = true,
-                    nvimtree = true,
-                    treesitter = true,
-                    notify = false,
-                    mini = {
-                        enabled = true,
-                        indentscope_color = "",
-                    },
-                    -- For more plugins integrations please scroll down (https://github.com/catppuccin/nvim#integrations)
-                },
-            })
-
-            -- setup must be called before loading
-            vim.cmd.colorscheme "catppuccin"
-        end
-    }
+    -- {
+    --     "EdenEast/nightfox.nvim",
+    --     config = function()
+    --         require('nightfox').setup({
+    --             options = {
+    --                 -- Compiled file's destination location
+    --                 compile_path = vim.fn.stdpath("cache") .. "/nightfox",
+    --                 compile_file_suffix = "_compiled", -- Compiled file suffix
+    --                 transparent = false, -- Disable setting background
+    --                 terminal_colors = true, -- Set terminal colors (vim.g.terminal_color_*) used in `:terminal`
+    --                 dim_inactive = false, -- Non focused panes set to alternative background
+    --                 module_default = true, -- Default enable value for modules
+    --                 colorblind = {
+    --                     enable = false, -- Enable colorblind support
+    --                     simulate_only = false, -- Only show simulated colorblind colors and not diff shifted
+    --                     severity = {
+    --                         protan = 0, -- Severity [0,1] for protan (red)
+    --                         deutan = 0, -- Severity [0,1] for deutan (green)
+    --                         tritan = 0, -- Severity [0,1] for tritan (blue)
+    --                     },
+    --                 },
+    --                 styles = { -- Style to be applied to different syntax groups
+    --                     comments = "NONE", -- Value is any valid attr-list value `:help attr-list`
+    --                     conditionals = "NONE",
+    --                     constants = "NONE",
+    --                     functions = "NONE",
+    --                     keywords = "NONE",
+    --                     numbers = "NONE",
+    --                     operators = "NONE",
+    --                     strings = "NONE",
+    --                     types = "NONE",
+    --                     variables = "NONE",
+    --                 },
+    --                 inverse = { -- Inverse highlight for different types
+    --                     match_paren = false,
+    --                     visual = false,
+    --                     search = false,
+    --                 },
+    --                 modules = { -- List of various plugins and additional options
+    --                     -- ...
+    --                 },
+    --             },
+    --             palettes = {},
+    --             specs = {},
+    --             groups = {},
+    --         })
+    --
+    --         -- setup must be called before loading
+    --         vim.cmd("colorscheme nordfox")
+    --     end
+    -- },
+    -- {
+    --     "sainnhe/gruvbox-material",
+    --     config = function ()
+    --         vim.cmd.colorscheme('gruvbox-material')
+    --     end
+    -- }
 }

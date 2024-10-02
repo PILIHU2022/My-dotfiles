a12ed4eb664e7a33c6a4f6c3b668d6aac9b09cd4
PILIHU2022 <2812167783@qq.com>
2024-10-02 14:16:36 +0800

    Neovim-cmp:Added conform and none-ls. Format codes. Document will change.

---
+++ .config/nvim-use-cmp/lua/plugins/conform.lua
@@ -0,0 +1,25 @@
+-- 安装和配置conform
+return {
+    {
+        "stevearc/conform.nvim", -- 安装插件
+        config = function()
+            -- 配置conform，使用black和isort格式化Python
+            require("conform").setup({
+                formatters_by_ft = {
+                    -- python = { "black" },
+                    python = { "black" },
+                    lua = { 'lua-ls' },
+                },
+            })
+
+            -- 自动格式化配置
+            -- vim.api.nvim_create_autocmd("BufWritePre", {
+            --     pattern = "*.py",
+            --     callback = function(args)
+            --         require("conform").format({ bufnr = args.buf })
+            --     end,
+            -- })
+        end,
+    }
+}
+

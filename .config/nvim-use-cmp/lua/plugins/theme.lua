7b754989c4ae5e1cdfc0c57c721c29a8515995e2
PILIHU2022 <2812167783@qq.com>
2024-11-17 15:06:46 +0800

    Zsh(zi_init):Change

--- .config/nvim-use-cmp/lua/plugins/theme.lua
+++ .config/nvim-use-cmp/lua/plugins/theme.lua
@@ -5,10 +5,20 @@ return {
     --         vim.cmd [[colorscheme tokyonight-storm]]
     --     end
     -- },
+    -- {
+    --     "sainnhe/gruvbox-material",
+    --     config = function ()
+    --         vim.cmd.colorscheme('gruvbox-material')
+    --     end
+    -- }
     {
-        "sainnhe/gruvbox-material",
-        config = function ()
-            vim.cmd.colorscheme('gruvbox-material')
-        end
+        'projekt0n/github-nvim-theme',
+        name = 'github-theme',
+        lazy = false, -- make sure we load this during startup if it is your main colorscheme
+        priority = 1000, -- make sure to load this before all the other start plugins
+        config = function()
+            require('github-theme').setup({})
+            vim.cmd('colorscheme github_dark_dimmed')
+        end,
     }
 }

8acf0d0603e2cef0e70d805f085febe9ed617404
PILIHU2022 <2812167783@qq.com>
2024-11-17 15:07:28 +0800

    Zsh(zi_load):Change

--- .config/zsh.d/zi.d/zi_config/2_zi_load.zsh
+++ .config/zsh.d/zi.d/zi_config/2_zi_load.zsh
@@ -3,8 +3,8 @@
 
 source "${ZI[BIN_DIR]}/zi.zsh"    # ## LOAD ZI
 
-zstyle ':completion:*' menu select
-zstyle ':completion:*' list-colors ${(s.:.)LS_COLORS}
+# zstyle ':completion:*' menu select
+# zstyle ':completion:*' list-colors ${(s.:.)LS_COLORS}
 autoload -Uz _zi
 (( ${+_comps} )) && _comps[zi]=_zi
 

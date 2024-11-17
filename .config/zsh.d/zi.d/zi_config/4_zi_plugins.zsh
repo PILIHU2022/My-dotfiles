4ba6f36721ec1de83b78956559bcea3f1a5dac46
PILIHU2022 <2812167783@qq.com>
2024-11-17 15:08:40 +0800

    Zsh(zi_plugins):Change

--- .config/zsh.d/zi.d/zi_config/4_zi_plugins.zsh
+++ .config/zsh.d/zi.d/zi_config/4_zi_plugins.zsh
@@ -1,18 +1,22 @@
 zi light-mode for \
-  z-shell/z-a-meta-plugins \
-  junegunn/fzf \
-  @annexes+ \
-  @zsh-users+fast \
-  Aloxaf/fzf-tab
-  # @romkatv
+    Aloxaf/fzf-tab\
+    junegunn/fzf\
+    zsh-users/zsh-completions\
+    zsh-users/zsh-autosuggestions\
+    z-shell/F-Sy-H
+    # zsh-users/zsh-syntax-highlighting 
+    # z-shell/z-a-meta-plugins\ 
+    # @annexes+\ 
+    # @zsh-users+fast\
+    # @romkatv
 
 # zsh-vi-mode
 # https://github.com/jeffreytse/zsh-vi-mode
 zi ice depth=1;zi light jeffreytse/zsh-vi-mode     # ## zsh-vi-mode
 
 # zoxide
-zi has'zoxide' wait lucid for z-shell/zsh-zoxide   # ## 需要 zoxide 二进制文件, 如果存在则加载
+# zi has'zoxide' wait lucid for z-shell/zsh-zoxide   # ## 需要 zoxide 二进制文件, 如果存在则加载
 
 #  H-S-MW
 # https://github.com/z-shell/H-S-MW
-zi ice wait lucid; zi light z-shell/H-S-MW
+# zi ice wait lucid; zi light z-shell/H-S-MW

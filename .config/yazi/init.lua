471ac87a561e2cda9f7dffe15435e89e03f1edf7
PILIHU2022 <2812167783@qq.com>
2024-12-21 17:00:15 +0800

    Yazi(Configuration&plugins):Add

---
+++ .config/yazi/init.lua
@@ -0,0 +1,7 @@
+THEME.git = THEME.git or {}
+THEME.git.deleted_sign = "✘"
+THEME.git.updated_sign = "U"
+THEME.git.added_sign = "+"
+THEME.git.untracked_sign = "?"
+THEME.git.modified_sign = "!"
+require("git"):setup()

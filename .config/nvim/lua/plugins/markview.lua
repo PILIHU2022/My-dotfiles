f0c558eda7c16cbc6ddc2352b4b16dca859c9767
PILIHU2022 <Spark-CN@outlook.com>
2025-02-03 13:30:01 +0800

    Neovim(markview):Add options

--- .config/nvim/lua/plugins/markview.lua
+++ .config/nvim/lua/plugins/markview.lua
@@ -1,10 +1,16 @@
 -- Markdown Preview
 return {
   "OXY2DEV/markview.nvim",
-  lazy = false, -- Recommended
-  -- ft = "markdown" -- If you decide to lazy-load anyway
+  lazy = true, -- Recommended
+  ft = "markdown", -- If you decide to lazy-load anyway
   dependencies = {
     "nvim-treesitter/nvim-treesitter",
     "nvim-tree/nvim-web-devicons",
   },
+  config = function()
+    require("markview").setup({
+      preview = { enable = false },
+      border = "rounded",
+    })
+  end,
 }

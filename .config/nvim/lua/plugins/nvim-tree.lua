fed2160a85bb33c46edc95358e9dd1fc07839c8b
PILIHU2022 <Spark-CN@outlook.com>
2025-07-04 15:41:18 +0800

    Neovim(nvim-tree):Unique lualine

--- .config/nvim/lua/plugins/nvim-tree.lua
+++ .config/nvim/lua/plugins/nvim-tree.lua
@@ -1,10 +1,4 @@
 return {
-  {
-    "nvim-lualine/lualine.nvim",
-    config = function()
-      require("lualine").setup()
-    end,
-  },
   -- {
   --     "nvim-tree/nvim-tree.lua",
   --     version = "*",

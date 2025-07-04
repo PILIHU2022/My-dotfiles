24ee16daa40956c24bd92c89fc9a4c9bfc20201b
PILIHU2022 <Spark-CN@outlook.com>
2025-07-04 15:42:07 +0800

    Zsh(plugins):fzf-tab configuration test

--- .config/zsh.d/zi.d/zi_config/5_zi_plugins_conf.zsh
+++ .config/zsh.d/zi.d/zi_config/5_zi_plugins_conf.zsh
@@ -63,6 +63,7 @@ zstyle ':completion:*:descriptions' format '[%d]'
 zstyle ':completion:*' list-colors ${(s.:.)LS_COLORS}
 # force zsh not to show completion menu, which allows fzf-tab to capture the unambiguous prefix
 zstyle ':completion:*' menu no
+zstyle ':completion:commands' menu no
 # preview directory's content with eza when completing cd
 zstyle ':fzf-tab:complete:cd:*' fzf-preview 'eza -1 --color=always $realpath'
 

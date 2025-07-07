060ca1e2ef2a3d3713e44fb41902fef2fc134cd4
PILIHU2022 <Spark-CN@outlook.com>
2025-07-07 14:26:57 +0800

    Neovim(lualine):Something deprecated

--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -3,7 +3,7 @@ local LSP_indicator = {
   function()
     local msg = "None"
     local buf_ft = vim.api.nvim_buf_get_option(0, "filetype")
-    local clients = vim.lsp.get_active_clients()
+    -- local clients = vim.lsp.get_active_clients()
 
     if next(clients) == nil then
       return msg

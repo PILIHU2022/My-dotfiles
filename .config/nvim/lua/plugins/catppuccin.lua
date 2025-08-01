d1c6e30f846a92ff12214c0be22f269e1382a26f
PILIHU2022 <Spark-CN@outlook.com>
2025-08-01 15:31:37 +0800

    feat(nvim):`catppuccin` add highlighting group for `blink.cmp`

--- .config/nvim/lua/plugins/catppuccin.lua
+++ .config/nvim/lua/plugins/catppuccin.lua
@@ -1,23 +1,17 @@
 local highlight_groups = function(C)
   return {
     NormalFloat = { bg = C.none }, -- transparent
-    FloatBorder = { fg = C.lavender },
+    FloatBorder = { fg = C.sky },
     TabLineSel = { bg = C.red },
     Pmenu = { bg = C.none },
     PmenuSel = { bg = C.overlay0 },
 
     -- blink.cmp
-    BlinkCmpMenuBorder = { fg = C.yellow },
-    BlinkCmpDocBorder = { fg = C.rosewater },
+    BlinkCmpDoc = { link = "Pmenu" },
+    BlinkCmpDocBorder = { link = 'FloatBorder' },
     BlinkCmpMenu = { link = "Pmenu" },
+    BlinkCmpMenuBorder = { fg = C.blue },
     BlinkCmpMenuSelection = { link = "PmenuSel" },
-
-    -- nvim-cmp
-    -- change values in `winhighlight`
-    -- CmpMenuBorder = { fg = C.yellow },
-    -- CmpDocBorder = { fg = C.rosewater },
-    -- CmpBG = { link = "Pmenu" },
-    -- CmpSel = { link = "PmenuSel" },
 
     -- telescope
     -- https://github.com/nvim-telescope/telescope.nvim/wiki/
@@ -26,74 +20,56 @@ local highlight_groups = function(C)
   }
 end
 
-local config = {
-  flavor = "mocha", -- latte, frappe, macchiato, mocha
-  background = "mocha",
-  floating_border = "on",
-  -- transparent_background = true,
-  -- term_colors = true,
-  no_italic = false,
-  no_bold = false,
-  no_underline = false,
-  styles = {
-    comments = { "italic" },
-    onditionals = {},
-    loops = {},
-    functions = { "italic" },
-    keywords = {},
-    strings = {},
-    variables = {},
-    numbers = {},
-    booleans = { "bold" },
-    properties = {},
-    types = { "italic" },
-  },
-  integrations = {
-    cmp = true,
-    gitsigns = true,
-    indent_blankline = {
-      enabled = true,
-      colored_indent_levels = true,
-    },
-    lsp_trouble = true,
-    markdown = true,
-    neotree = true,
-    telescope = {
-      enabled = true,
-    },
-    treesitter_context = true,
-    which_key = true,
-    native_lsp = {
-      enable = true,
-      virtual_text = {
-        errors = { "bold" },
-        hints = { "italic" },
-        warnings = {},
-        information = {},
-      },
-      -- underlines = {
-      --   errors = { "underline" },
-      --   hints = { "underline" },
-      --   warnings = { "underline" },
-      --   information = { "underline" },
-      -- },
-      inlay_hints = {
-        background = true,
-      },
-    },
-  },
-  -- user defined highlight group
-  highlight_overrides = {
-    mocha = highlight_groups,
-  },
-}
-
 return {
   "catppuccin/nvim",
   name = "catppuccin",
   priority = 1000,
   config = function()
-    require("catppuccin").setup(config)
+    require("catppuccin").setup({
+      flavor = "mocha", -- latte, frappe, macchiato, mocha
+      background = {
+        light = "latte",
+        dark = "mocha",
+      },
+      floating_border = "on",
+      -- transparent_background = true,
+      -- term_colors = true,
+      no_italic = false,
+      no_bold = false,
+      no_underline = false,
+      styles = {
+        comments = { "italic" },
+        loops = {},
+        functions = { "italic" },
+        keywords = {},
+        strings = {},
+        variables = {},
+        numbers = {},
+        booleans = { "bold" },
+        properties = {},
+        types = { "italic" },
+      },
+      integrations = {
+        blink_cmp = true,
+        gitsigns = true,
+        indent_blankline = {
+          enabled = true,
+          colored_indent_levels = true,
+        },
+        lsp_trouble = true,
+        markdown = true,
+        neotree = true,
+        telescope = {
+          enabled = true,
+        },
+        treesitter_context = true,
+        which_key = true,
+      },
+
+      highlight_overrides = {
+        mocha = highlight_groups,
+      },
+    })
     vim.cmd.colorscheme("catppuccin-mocha")
   end,
 }

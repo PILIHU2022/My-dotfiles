f0ce503f8fbbb709e6301641bdb9074f4ee1aa91
PILIHU2022 <Spark-CN@outlook.com>
2025-08-03 14:50:20 +0800

    chore(nvim): change `todo-comment` plugin keyword highlight style

--- .config/nvim/lua/plugins/todo-comment.lua
+++ .config/nvim/lua/plugins/todo-comment.lua
@@ -3,7 +3,7 @@ return {
   dependencies = { "nvim-lua/plenary.nvim" },
   opts = {
 
-    signs = true, -- show icons in the signs column
+    signs = false, -- show icons in the signs column
     sign_priority = 8, -- sign priority
     -- keywords recognized as todo comments
     keywords = {
@@ -34,7 +34,7 @@ return {
       multiline_pattern = "^.", -- lua pattern to match the next multiline from the start of the matched keyword
       multiline_context = 10, -- extra lines that will be re-evaluated when changing a line
       before = "", -- "fg" or "bg" or empty
-      keyword = "wide", -- "fg", "bg", "wide", "wide_bg", "wide_fg" or empty. (wide and wide_bg is the same as bg, but will also highlight surrounding characters, wide_fg acts accordingly but with fg)
+      keyword = "bg", -- "fg", "bg", "wide", "wide_bg", "wide_fg" or empty. (wide and wide_bg is the same as bg, but will also highlight surrounding characters, wide_fg acts accordingly but with fg)
       after = "fg", -- "fg" or "bg" or empty
       pattern = [[.*<(KEYWORDS)\s*:]], -- pattern or table of patterns, used for highlighting (vim regex)
       comments_only = true, -- uses treesitter to match keywords in comments only
